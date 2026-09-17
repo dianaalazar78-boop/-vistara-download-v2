768296fc045c751e75759f87464b869d3821eb57
dianaalazar78-boop <dianaalazar78@gmail.com>
2026-09-17 18:19:29 +0700

    Change export syntax from default to module.exports

--- api/api/download.js
+++ api/api/download.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Only POST requests are allowed" });
   }
@@ -14,4 +14,4 @@ export default function handler(req, res) {
     message: "URL received successfully",
     url: url
   });
-}
+};
